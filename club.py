8c80dc859fc340bb2605fefbf3b77edab618dd3f
Programmer8316 <ashermon31@gmail.com>
2025-01-17 11:12:19 -0600

    Update club.py

--- club.py
+++ club.py
@@ -4,7 +4,15 @@ import csv
 #Custom error #1
 class PassError(Exception):
     def __init__(self, name):
-        self.name = name
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        self._name = name
 
     def __str__(self):
         return f"Unrecognizable pass token for {self.name}."
